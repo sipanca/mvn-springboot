4b117cba7f23b95bcb18c1c76863d617aedbd0ae
devops <panca.simanjuntak@asliri.id>
2023-08-14 14:29:41 +0700

    New update again

--- src/main/java/com/example/springboot/HelloController.java
+++ src/main/java/com/example/springboot/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
 	@GetMapping("/")
 	public String index() {
-		return "Greetings from Spring Boot!, Indonesia Merdeka";
+		return "Greetings from Spring Boot!";
 	}
 
 }
