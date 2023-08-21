11abcbd5e1122b313008235606a842ec14bb4489
devops <panca.simanjuntak@asliri.id>
2023-08-21 14:30:55 +0700

    Testing

--- src/main/java/com/example/springboot/HelloController.java
+++ src/main/java/com/example/springboot/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
 	@GetMapping("/")
 	public String index() {
-		return "Greetings from Spring Boot! - DevOps ASLI RI";
+		return "Greetings from Spring Boot! - Hello World !!!";
 	}
 
 }
